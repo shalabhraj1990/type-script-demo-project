a642721302c01db22bb0421e3364e310fdbc6b71
shalabh raj <shalabhraj@shalabhs-MacBook-Air.local>
2023-04-01 15:04:13 +0530

    features added:

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,22 @@
-function startGame() {
-  // starting a new game
-  var messagesElement = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
-}
+import { Player } from './player';
+import { Game } from './game';
+import * as Helpers from './utility';
 
-document.getElementById('startGame')!.addEventListener('click', startGame);
+let newGame: Game;
+
+// add click handler to the start game button
+document.getElementById('startGame')!.addEventListener('click', () => {
+  const player: Player = new Player();
+  player.name = Helpers.getValue('playername');
+
+  const problemCount: number = Number(Helpers.getValue('problemCount'));
+  const factor: number = Number(Helpers.getValue('factor'));
+
+  newGame = new Game(player, problemCount, factor);
+  newGame.displayGame();
+});
+
+// add click handler to the calculate score button
+document.getElementById('calculate')!.addEventListener('click', () => {
+  newGame.calculateScore();
+});
